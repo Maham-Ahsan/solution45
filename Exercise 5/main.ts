a3c9f13e165423de4d58bb213d3ac9413cb31a4b
Toobailyas <137196562+Toobailyas@users.noreply.github.com>
2024-03-04 01:18:46 +0500

    upload files

---
+++ Exercise 5/main.ts
@@ -0,0 +1,11 @@
+// storing qoute
+let qoute:string = "Everybody is a genious.But if you judge a Fish by its ability to climb a tree.It will live it's whole life beliving that it is stupid"
+
+// Storing Famous Person's name 
+let famous_person = "Albert Einstein";
+
+// storing message 
+let message =`${famous_person} Once said that, ${qoute}`;
+
+//printing the message
+console.log(message)
