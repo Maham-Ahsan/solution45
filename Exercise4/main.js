4109b959c369d486b975b5db0e901af6c10aa8e3
Toobailyas <137196562+Toobailyas@users.noreply.github.com>
2024-03-03 11:44:33 +0500

    first commit

---
+++ Exercise4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "Necessity is the mother of invention";
+let author = "plato";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ Exercise4/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "Necessity is the mother of invention"
+
+let author:string = "plato";
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
